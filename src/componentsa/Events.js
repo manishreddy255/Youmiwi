094d0c3c7473f6241a8fcc463b499fcb09a71061
Manish Reddy <manishreddy255@gmail.com>
2022-05-26 16:10:59 +0530

    added form and events page

--- src/componentsa/Events.js
+++ src/componentsa/Events.js
@@ -1,8 +1,73 @@
+import { Group, Image, Text, Container, Divider } from "@mantine/core"
+import republic from "../events/republic-22.jpeg"
+import magicofMeditation from "../events/magicOfMed.jpeg"
+import yogasession from "../events/yogasession.jpeg"
+import shashikanth from "../events/shashikanth.jpeg"
+
 const Events = () => {
 
     return (
         <>
-        <h1> these are events</h1>
+            <div className="pl-[10vw]">
+                <Text className="text-[1.5em] text-slate-600 text-left" weight={500}>Events</Text>
+            </div>
+            <Container className="mt-[7vh]" >
+
+
+                <Group className="grid  grid-cols-1 md:grid-cols-2 gap-6 mb-[5vh] text-[1em] ">
+                    <div className="flex justify-center md:justify-start items-center ">
+
+                        <Image
+                            className="w-[30vh] h-auto object-cover flex justify-center"
+                            src={republic}
+                        />
+                    </div>
+                    <Text className="text-[1.3em] w-[15em] text-center md:text-left">
+                        A session on the eve of 73rd Republic day with most beautiful patriotic performances from our talented peeps
+                    </Text>
+                </Group>
+                <Divider my="sm" />
+
+
+                <Group className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-[5vh] mt-[5vh]">
+                    <Text className="text-[1.3em] w-[15em] text-center md:text-left">
+                        A session by Captain Shashikant about carrier in Indian Army Aviation for the young and talented aspirants
+                    </Text>
+                    <div className="flex justify-center md:justify-start items-center">
+
+                        <Image
+                            className="w-[30vh] h-auto object-cover"
+                            src={shashikanth}
+                        />
+                    </div>
+                </Group>
+                <Divider my="sm" />
+                <Group className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-[5vh] mt-[5vh]">
+                    <div className="flex justify-center md:justify-start items-center">
+
+                        <Image
+                            className="w-[30vh] h-auto object-cover"
+                            src={magicofMeditation}
+                        />
+                    </div>
+                    <Text className="text-[1.3em] w-[15em] text-center md:text-left">
+                        A session with the 6 time guinness book of World Records holder Master Eshwar on his spiritual and professional journey 😍
+                    </Text>
+                </Group>
+                <Divider my="sm" />
+                <Group className="grid grid-cols-1  md:grid-cols-2   gap-6 mb-[5vh]">
+                    <Text className="text-[1.3em] w-[15em] text-center md:text-left">
+                        Taking a step towards a fit and healthy society, We begun our Yoga sessions under the guidance of a professional trainer
+                    </Text>
+                    <div className="flex justify-center md:justify-start items-center">
+
+                        <Image
+                            className="w-[30vh] h-auto object-cover"
+                            src={yogasession}
+                        />
+                    </div>
+                </Group>
+            </Container>
         </>
     )
 }
--- src/componentsa/RegisterForm.js
+++ src/componentsa/RegisterForm.js
@@ -1,8 +1,140 @@
+import { TextInput, Group, Button, Chips, Chip, Text , InputWrapper} from "@mantine/core";
+import { At, PhoneCall, Location } from 'tabler-icons-react';
+import { useState } from 'react';
 const RegisterFrom = () => {
 
-    return(
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mailError, setMailError] = useState('');
+    const [phone, setPhone] = useState('');
+    const [phoneError, setPhoneError] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [ageGroup, setAgeGroup] = useState('');
+
+    const mailValidate = (val) => {
+        console.log(val)
+        if (val === "") {
+            setMailError('Email is required')
+        } else if (/^\S+@\S+$/.test(val)) {
+            setMailError("")
+        } else {
+            setMailError("Invalid email")
+        }
+    }
+
+    const numberValidate = (val) => {
+        console.log(val)
+        if (val === "") {
+            setPhoneError('enter phone number')
+        } else if (/^\d{10}$/.test(val)) {
+            setPhoneError("")
+        } else {
+            setPhoneError("Invalid phone number")
+        }
+    }
+
+    const submitData = (event) => {
+        event.preventDefault();
+        console.log(
+            fullName,
+            email,
+            phone,
+            city,
+            state,
+            ageGroup
+        )
+    }
+
+    return (
         <>
-        <h1> Form </h1>
+            <div className="pl-[5vw] pr-[5vw]  pb-[5vw]">
+
+                <Text>Register</Text>
+                <Text>Get access to our WhatsApp community by regestring</Text>
+                <form onSubmit={submitData} className="w-[100%] md:w-[70%] m-auto " >
+                    <TextInput
+                        placeholder="Your name"
+                        label="Full name"
+                        required
+                        value={fullName}
+                        className=" text-left "
+                        size="md"
+                        onChange={(e) => setFullName(e.target.value)}
+                    />
+                    <TextInput
+                        icon={<At />}
+                        required
+                        label="Email"
+                        placeholder="your@email.com"
+                        error={mailError}
+                        className=" text-left "
+                        size="md"
+                        value={email}
+                        onChange={(e) => {
+
+                            setEmail(e.target.value)
+                            mailValidate(e.target.value)
+                        }}
+                    />
+                    <TextInput
+                        icon={<PhoneCall />}
+                        required
+                        label="Phone Number"
+                        error={phoneError}
+                        className=" text-left "
+                        size="md"
+                        value={phone}
+                        onChange={(e) => {
+
+                            setPhone(e.target.value)
+                            numberValidate(e.target.value)
+                        }}
+                    />
+                    <Group className="flex">
+                        <TextInput
+                            icon={<Location />}
+                            required
+                            label="State"
+                            className=" text-left "
+                            size="md"
+                            value={state}
+                            onChange={(e) => {
+
+                                setState(e.target.value)
+                            }}
+                        />
+                        <TextInput
+                            icon={<Location />}
+                            required
+                            label="City"
+                            className=" text-left "
+                            size="md"
+                            value={city}
+                            onChange={(e) => {
+
+                                setCity(e.target.value)
+                            }}
+                        />
+                    </Group>
+                    <InputWrapper label="Age Group"
+                        className=" text-left "
+                        size="md"
+                        required>
+                        <Chips className="mt-[1vh]" value={ageGroup} onChange={setAgeGroup}
+                        >
+                            <Chip value="Below 12yrs ">Below 12yrs </Chip>
+                            <Chip value="13 to 17 yrs">13 to 17 yrs</Chip>
+                            <Chip value="18 to 25 yrs">18 to 25 yrs</Chip>
+                        </Chips>
+                    </InputWrapper>
+
+                    <Button className=" bg-[#228be6] mt-[2vh]" >
+                        Register
+                    </Button>
+                </form>
+        </div>
+            
         </>
     )
 }
