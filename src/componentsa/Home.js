e084b06e88e1be6be27a3073c621d2b71a6c49c3
Manish Reddy <manishreddy255@gmail.com>
2022-05-26 10:30:56 +0530

    added parents

--- src/componentsa/Home.js
+++ src/componentsa/Home.js
@@ -1,5 +1,6 @@
-import { Title, Text } from "@mantine/core";
-
+import { Title, Text, Image } from "@mantine/core";
+import parent1 from "../images/parent1.png"
+import parent2 from "../images/parent-2.png"
 const Home = () => {
 
     return (
@@ -14,11 +15,33 @@ const Home = () => {
                     YouMeWi(Youth Meditation Wing) is a not-for-profit organization in India set up on 26th May of 2021 as a part of Spiritual tablets. Our mission is to impart meditation into the daily lives of children, teenagers, and youth through various forms of arts.
                     <br />
                     <br />
+                    The influence of meditation on our daily lives is enormous. Meditation enhances a child's focus. It cultivates mental stability and enriches inner peace in teens.
+                    <br />
+                    <br />
 
                     As a part of our mission, we conduct online interactive sessions for yoga, music, painting, and dance(Kuchipudi) for children, teenagers, and youth. We also organize weekly fun-packed game sessions and cultural programs where people display their unique skills and interests.
                     <br />
                     <br />
                     We also promote vegetarianism for a happy, healthy, and holistic life.</Text>
+            </div>
+            <div className="grid  grid-cols-1 text-[0.9em] gap-[4vh]  pl-[5vw] pr-[10vw] pt-[5vw]  pb-[5vw] " >
+                <Title className=" text-center md:text-left " order={1} style={{ fontSize: "2em" }}>Parent Organization </Title>
+                <div className=" flex flex-col md:flex-row justify-center items-center ">
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }} className=" pb-[4vh] " >
+                        <Image
+                            radius="md"
+                            src={parent1}
+                            alt="Random unsplash image"
+                        />
+                    </div>
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }} className="flex justify-center items-center pb-[4vh">
+                        <Image
+                            radius="md"
+                            src={parent2}
+                            alt="Random unsplash image"
+                        />
+                    </div>
+                </div>
             </div>
         </>
     )
